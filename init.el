2312648cfbe3aa3b9db20c4d248211317513b883
Digitalworldkamikaze <49838304+Digitalworldkamikaze@users.noreply.github.com>
2019-11-29 06:10:29 +0000

    Add files via upload

--- init.el
+++ init.el
@@ -5,6 +5,23 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(setq inhibit-startup-message t)
+
+;; Load theme
+(load-theme 'zerodark t)
+
+;; Save point position between sessions
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file (expand-file-name ".places" user-emacs-directory))
+
+;; Package archives
+(setq package-archves
+      (append (eval (car (get 'package-archives 'standard-value)))
+              '(("org" . "https://orgmode.org/elpa/")
+                ("melpa" . "https://melpa.org/packages/")
+                ("elpa" . "https://elpa.gnu.org/packages")
+(package-initialize))))
 
 ;; Setting-up global keys
 (global-set-key (kbd "M-o") 'other-window)
@@ -16,16 +33,29 @@
 
 ;; Custom-set variables and faces
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(bookmark-default-file "~/.emacs.d/bookmarks")
+ '(custom-safe-themes
+   (quote
+    ("1068ae7acf99967cc322831589497fee6fb430490147ca12ca7dd3e38d9b552a" "82358261c32ebedfee2ca0f87299f74008a2e5ba5c502bde7aaa15db20ee3731" default)))
  '(display-line-numbers (quote relative))
  '(global-subword-mode t)
  '(ido-enable-flex-matching t)
  '(indent-tabs-mode nil)
- '(package-selected-packages (quote (helm use-package)))
+ '(package-selected-packages
+   (quote
+    (gnu-elpa-keyring-update pdf-tools zerodark-theme solarized-theme nord-theme evil helm use-package)))
  '(sentence-end-double-space nil)
  '(tab-width 4)
  '(winner-mode t))
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "PfEd" :family "Iosevka")))))
 
 ;; Cool autocompletion with helm
@@ -35,6 +65,17 @@
 (ido-mode 1)
 (setq ido-everywhere t)
 (setq ido-enable-flex-matching t)
+
+(add-hook 'ido-setup-hook
+ (lambda ()
+   ;; Go straight home
+   (define-key ido-file-completion-map
+     (kbd "~")
+     (lambda ()
+       (interactive)
+       (if (looking-back "/")
+           (insert "~/")
+         (call-interactively 'self-insert-command))))))
 
 ;; Enable some "confusing" functions
 (put 'scroll-left 'disabled nil)
@@ -54,4 +95,4 @@
              buffer-file-name))))
 
 ;; Dired file-manager functions
-(require 'dired-x)   
+(require 'dired-x)
