6e8625db81cc795377e36a04acb2bdd2839de1a1
Digitalworldkamikaze <49838304+Digitalworldkamikaze@users.noreply.github.com>
2020-01-25 16:40:16 +0500

    Minor init.el updates

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 ;; package.el stuff
 (package-initialize)			
 
+;; Those little guys in the end of a buffer
+(setq-default indicate-empty-lines t)
+
 ;; Remove some GUI shit
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -8,12 +11,7 @@
 (setq inhibit-startup-message t)
 
 ;; Load theme
-(load-theme 'zerodark t)
-
-;; Save point position between sessions
-(require 'saveplace)
-(setq-default save-place t)
-(setq save-place-file (expand-file-name ".places" user-emacs-directory))
+(load-theme 'doom-city-lights t)
 
 ;; Package archives
 (defvar gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
@@ -24,6 +22,31 @@
                          ("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")))
 
+;; Set a backup directory
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+
+;; use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Trying packages
+(use-package try
+             :ensure t)
+
+;; Which key?
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
+
+;; Vim-mode for everything
+(use-package evil
+  :ensure t
+  :config (evil-mode))
+
+;; y or n instead of 'yes' or 'no'
+(fset 'yes-or-no-p 'y-or-n-p)
 
 ;; Setting-up global keys
 (global-set-key (kbd "M-o") 'other-window)
@@ -39,17 +62,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
  '(bookmark-default-file "~/.emacs.d/bookmarks")
  '(custom-safe-themes
    (quote
-    ("1068ae7acf99967cc322831589497fee6fb430490147ca12ca7dd3e38d9b552a" "82358261c32ebedfee2ca0f87299f74008a2e5ba5c502bde7aaa15db20ee3731" default)))
+    ("51956e440cec75ba7e4cff6c79f4f8c884a50b220e78e5e05145386f5b381f7b" "0809c08440b51a39c77ec5529f89af83ab256a9d48107b088d40098ce322c7d8" "7c4cfa4eb784539d6e09ecc118428cd8125d6aa3053d8e8413f31a7293d43169" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "ca849ae0c889eb918785cdc75452b1e11a00848a5128a95a23872e0119ccc8f4" "7f791f743870983b9bb90c8285e1e0ba1bf1ea6e9c9a02c60335899ba20f3c94" "1068ae7acf99967cc322831589497fee6fb430490147ca12ca7dd3e38d9b552a" "82358261c32ebedfee2ca0f87299f74008a2e5ba5c502bde7aaa15db20ee3731" default)))
  '(display-line-numbers (quote relative))
  '(global-subword-mode t)
  '(ido-enable-flex-matching t)
  '(indent-tabs-mode nil)
  '(package-selected-packages
    (quote
-    (gnu-elpa-keyring-update pdf-tools zerodark-theme solarized-theme nord-theme evil helm use-package)))
+    (org-bullets which-key try doom-themes minimap gnu-elpa-keyring-update pdf-tools zerodark-theme solarized-theme nord-theme evil helm use-package)))
  '(sentence-end-double-space nil)
  '(tab-width 4)
  '(winner-mode t))
@@ -79,11 +104,6 @@
            (insert "~/")
          (call-interactively 'self-insert-command))))))
 
-;; Enable some "confusing" functions
-(put 'scroll-left 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'dired-find-alternate-file 'disabled nil)
-
 ;; Point-and-mark stuff
 (delete-selection-mode)
 
@@ -98,3 +118,20 @@
 
 ;; Dired file-manager functions
 (require 'dired-x)
+
+;; Org-mode functions
+;; Bullets
+(use-package org-bullets
+  :ensure t)
+
+;; Set colours for priorities
+(setq org-priority-faces '((?A . (:foreground "#F0DFAF" :weight bold))
+                           (?B . (:foreground "LightSteelBlue"))
+                           (?C . (:foreground "OliveDrab"))))
+
+;; Capture todo items using C-c c t
+(define-key global-map (kbd "C-c c") 'org-capture)
+(setq org-capture-templates
+      '(("t" "todo" entry (file+headline "/Users/bjm/todo.org" "Tasks")
+         "* TODO [#A] %?")))
+
