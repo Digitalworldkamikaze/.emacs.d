c8ff546680dd6baadaa62c9f416c7c01e3b763a9
Digitalworldkamikaze <49838304+Digitalworldkamikaze@users.noreply.github.com>
2020-04-24 21:14:47 +0500

    Some cool changes to modal layout, interface settings, and most importantly, avy.

--- init.el
+++ init.el
@@ -1,5 +1,11 @@
 ;; initializing package.el
 (package-initialize)
 
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6
+      file-name-handler-alist nil
+      site-run-file nil)
+
 ;; grabbing config from the org file
 (org-babel-load-file "~/.emacs.d/README.org")
+(put 'upcase-region 'disabled nil)
